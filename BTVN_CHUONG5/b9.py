a11578add355e4e4ce7ff022c58f487483733919
nguyenducthanh10032005 <ndthanh.dhkl17a2hn@sv.uneti.edu.vn>
2024-12-25 20:21:02 +0700

    BTVN_CHUONG5

---
+++ BTVN_CHUONG5/b9.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+df = pd.read_csv(r"F:/Bài Tập Chương PYTHON/Bài tập chương 2/BAI TAP PYTHON/BTVN_CHUONG5/region.csv") 
+
+
+print(df)
+print("Nội dung của file region.csv:")
